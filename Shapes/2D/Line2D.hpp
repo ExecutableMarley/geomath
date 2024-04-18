507557e8f5fae28817df298bcefd03992e02f9b7
Marley <marley.arns@yahoo.de>
2024-04-18 15:06:46 +0200

    Added Line2D class.

---
+++ Shapes/2D/Line2D.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <math.h>
+
+#include "Vector2D.hpp"
+
+namespace Utility
+{
+
+namespace Math
+{
+
+class Line2D
+{
+public:
+    Vector2D m_start;
+    Vector2D m_end;
+
+    Line2D() : m_start(), m_end() {}
+
+    Line2D(Vector2D startPoint, Vector2D endPoint) : m_start(startPoint), m_end(endPoint) {}
+
+    Line2D(Vector2D startPoint, Vector2D direction, float length) : m_start(startPoint), m_end(startPoint + direction.createNormalized() * length) {}
+
+    float length()
+    {
+        return (m_start - m_end).length();
+    }
+
+    Vector2D direction()
+    {
+        return (m_end - m_start).normalize();
+    }
+};
+
+} // namespace Math
+
+} // namespace Utility
