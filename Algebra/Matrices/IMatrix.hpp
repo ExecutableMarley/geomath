c158b7fae54bc58018e1a8e410f9512794067474
Marley <marley.arns@yahoo.de>
2024-05-15 13:07:36 +0200

    feat: Add IMatrix and Matrix classes for matrix operations

---
+++ Algebra/Matrices/IMatrix.hpp
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) Marley Arns
+ * Licensed under the MIT License.
+*/
+
+#pragma once
+
+#include <math.h>
+
+namespace Utility
+{
+
+namespace Math
+{
+
+class IMatrix
+{
+public:
+    virtual ~IMatrix() = default;
+
+    virtual size_t rows() const = 0;
+    virtual size_t columns() const = 0;
+
+    virtual float& operator()(size_t row, size_t column) = 0;
+    virtual const float& operator()(size_t row, size_t column) const = 0;
+
+
+    IMatrix& operator+=(const IMatrix& other)
+    {
+        for (int i = 0; i < rows(); i++)
+            for (int j = 0; j < columns(); j++)
+                (*this)(i, j) += other(i, j);
+        return *this;
+    }
+
+    IMatrix& operator+=(float scalar)
+    {
+        for (int i = 0; i < rows(); i++)
+            for (int j = 0; j < columns(); j++)
+                (*this)(i, j) += scalar;
+        return *this;
+    }
+
+    IMatrix& operator-=(const IMatrix& other)
+    {
+        for (int i = 0; i < rows(); i++)
+            for (int j = 0; j < columns(); j++)
+                (*this)(i, j) -= other(i, j);
+        return *this;
+    }
+
+    IMatrix& operator-=(float scalar)
+    {
+        for (int i = 0; i < rows(); i++)
+            for (int j = 0; j < columns(); j++)
+                (*this)(i, j) -= scalar;
+        return *this;
+    }
+
+    IMatrix& operator*=(float scalar)
+    {
+        for (int i = 0; i < rows(); i++)
+            for (int j = 0; j < columns(); j++)
+                (*this)(i, j) *= scalar;
+        return *this;
+    }
+
+    IMatrix& operator/=(float scalar)
+    {
+        for (int i = 0; i < rows(); i++)
+            for (int j = 0; j < columns(); j++)
+                (*this)(i, j) /= scalar;
+        return *this;
+    }
+};
+
+
+} // namespace Math
+
+} // namespace Utility
---
+++ Algebra/Matrices/Matrix.hpp
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) Marley Arns
+ * Licensed under the MIT License.
+*/
+
+#pragma once
+
+#include <math.h>
+#include <vector>
+#include <stdexcept>
+
+#include "IMatrix.hpp"
+
+namespace Utility
+{
+
+namespace Math
+{
+
+class Matrix : public IMatrix
+{
+    std::vector<std::vector<float>> m_data;
+    size_t m_rows;
+    size_t m_columns;
+public:
+
+    Matrix() : m_data(), m_rows(0), m_columns(0) {}
+
+    Matrix(size_t rows, size_t columns, float initValue = 0.f) : m_data(rows, std::vector<float>(columns, initValue)) 
+    {
+        this->m_rows = rows;
+        this->m_columns = columns;
+    }
+
+    Matrix(size_t rows, size_t columns, const std::vector<float> &data) : m_data(rows, std::vector<float>(columns))
+    {
+        if (data.size() != rows * columns)
+        {
+            throw std::invalid_argument("Matrix data size does not match the number of rows and columns.");
+        }
+
+        this->m_rows = rows;
+        this->m_columns = columns;
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                m_data[i][j] = data[i * columns + j];
+            }
+        }
+    }
+
+    Matrix(const Matrix &other) : m_data(other.m_data), m_rows(other.m_rows), m_columns(other.m_columns) {}
+
+    Matrix(const IMatrix &other)
+    {
+        m_rows = other.rows();
+        m_columns = other.columns();
+        m_data = std::vector<std::vector<float>>(m_rows, std::vector<float>(m_columns, 0));
+        for (int i = 0; i < m_rows; i++)
+            for (int j = 0; j < m_columns; j++)
+                m_data[i][j] = other(i, j);
+    }
+
+    //Overriding the IMatrix interface
+
+    size_t rows()    const override { return m_rows;}
+
+    size_t columns() const override { return m_columns;}
+
+    float& operator()(size_t row, size_t column) override
+    {
+        return m_data[row][column];
+    }
+
+    const float& operator()(size_t row, size_t column) const override
+    {
+        return m_data[row][column];
+    }
+};
+
+} // namespace Math
+
+} // namespace Utility
