e35b20ee00649f21a93266fd03d8e1633d3b4dc4
Marley <marley.arns@yahoo.de>
2024-05-17 13:19:59 +0200

    refactor: RotationMatrix and ViewMatrix now inherit from fixed size Matrices

--- Algebra/RotationMatrix.hpp
+++ Algebra/RotationMatrix.hpp
@@ -10,61 +10,40 @@
 #include "../CommonMath.hpp"
 #include "Geometry/Vector3D.hpp"
 
+#include "Matrices/Matrix3x3.hpp"
+
 namespace Utility
 {
 
 namespace Math
 {
-//Matrix3x3 Specialization
-class RotationMatrix
+
+class RotationMatrix : public Matrix3x3
 {
-    float m[3][3];
+public:
+    using Matrix3x3::Matrix3x3;
 
     RotationMatrix()
     {
-        m[0][0] = 1.0f;
-        m[0][1] = 0.0f;
-        m[0][2] = 0.0f;
+        m_data[0][0] = 1.0f;
+        m_data[0][1] = 0.0f;
+        m_data[0][2] = 0.0f;
 
-        m[1][0] = 0.0f;
-        m[1][1] = 1.0f;
-        m[1][2] = 0.0f;
+        m_data[1][0] = 0.0f;
+        m_data[1][1] = 1.0f;
+        m_data[1][2] = 0.0f;
 
-        m[2][0] = 0.0f;
-        m[2][1] = 0.0f;
-        m[2][2] = 1.0f;
-    }
-
-    RotationMatrix(float m00, float m01, float m02,
-                   float m10, float m11, float m12,
-                   float m20, float m21, float m22)
-    {
-        m[0][0] = m00;
-        m[0][1] = m01;
-        m[0][2] = m02;
-
-        m[1][0] = m10;
-        m[1][1] = m11;
-        m[1][2] = m12;
-
-        m[2][0] = m20;
-        m[2][1] = m21;
-        m[2][2] = m22;
-    }
-
-    RotationMatrix(const RotationMatrix &other)
-    {
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
-                m[i][j] = other.m[i][j];
+        m_data[2][0] = 0.0f;
+        m_data[2][1] = 0.0f;
+        m_data[2][2] = 1.0f;
     }
 
     Vector3D transform(Vector3D &vec3) const
     {
         return Vector3D(
-            vec3.x * m[0][0] + vec3.y * m[0][1] + vec3.z * m[0][2],
-            vec3.x * m[1][0] + vec3.y * m[1][1] + vec3.z * m[1][2],
-            vec3.x * m[2][0] + vec3.y * m[2][1] + vec3.z * m[2][2]);
+            vec3.x * m_data[0][0] + vec3.y * m_data[0][1] + vec3.z * m_data[0][2],
+            vec3.x * m_data[1][0] + vec3.y * m_data[1][1] + vec3.z * m_data[1][2],
+            vec3.x * m_data[2][0] + vec3.y * m_data[2][1] + vec3.z * m_data[2][2]);
     }
 
     // Static functions
--- Algebra/ViewMatrix.hpp
+++ Algebra/ViewMatrix.hpp
@@ -11,77 +11,50 @@
 #include "Vector3D.hpp"
 #include "Vector4D.hpp"
 
+#include "Matrices/Matrix4x4.hpp"
+
 namespace Utility
 {
 
 namespace Math
 {
-// Matrix4x4 Specialization
-class ViewMatrix
+
+class ViewMatrix : public Matrix4x4
 {
-    float m[4][4];
+public:
+    using Matrix4x4::Matrix4x4;
 
     ViewMatrix()
     {
         for (int i = 0; i < 4; i++)
             for (int j = 0; j < 4; j++)
                 if (i == j)
-                    m[i][j] = 1;
+                    m_data[i][j] = 1;
                 else
-                    m[i][j] = 0;
-    }
-
-    ViewMatrix(float m00, float m01, float m02, float m03,
-               float m10, float m11, float m12, float m13,
-               float m20, float m21, float m22, float m23,
-               float m30, float m31, float m32, float m33)
-    {
-        m[0][0] = m00;
-        m[0][1] = m01;
-        m[0][2] = m02;
-        m[0][3] = m03;
-        m[1][0] = m10;
-        m[1][1] = m11;
-        m[1][2] = m12;
-        m[1][3] = m13;
-        m[2][0] = m20;
-        m[2][1] = m21;
-        m[2][2] = m22;
-        m[2][3] = m23;
-        m[3][0] = m30;
-        m[3][1] = m31;
-        m[3][2] = m32;
-        m[3][3] = m33;
-    }
-
-    ViewMatrix(const ViewMatrix &other)
-    {
-        for (int i = 0; i < 4; i++)
-            for (int j = 0; j < 4; j++)
-                m[i][j] = other.m[i][j];
+                    m_data[i][j] = 0;
     }
 
     Vector4D transform(const Vector4D &vec4) const
     {
         return Vector4D(
-            vec4.x * m[0][0] + vec4.y * m[0][1] + vec4.z * m[0][2] + vec4.w * m[0][3],
-            vec4.x * m[1][0] + vec4.y * m[1][1] + vec4.z * m[1][2] + vec4.w * m[1][3],
-            vec4.x * m[2][0] + vec4.y * m[2][1] + vec4.z * m[2][2] + vec4.w * m[2][3],
-            vec4.x * m[3][0] + vec4.y * m[3][1] + vec4.z * m[3][2] + vec4.w * m[3][3]);
+            vec4.x * m_data[0][0] + vec4.y * m_data[0][1] + vec4.z * m_data[0][2] + vec4.w * m_data[0][3],
+            vec4.x * m_data[1][0] + vec4.y * m_data[1][1] + vec4.z * m_data[1][2] + vec4.w * m_data[1][3],
+            vec4.x * m_data[2][0] + vec4.y * m_data[2][1] + vec4.z * m_data[2][2] + vec4.w * m_data[2][3],
+            vec4.x * m_data[3][0] + vec4.y * m_data[3][1] + vec4.z * m_data[3][2] + vec4.w * m_data[3][3]);
     }
 
     Vector4D transform(const Vector3D &vec3) const
     {
         return Vector4D(
-            vec3.x * m[0][0] + vec3.y * m[0][1] + vec3.z * m[0][2] + m[0][3],
-            vec3.x * m[1][0] + vec3.y * m[1][1] + vec3.z * m[1][2] + m[1][3],
-            vec3.x * m[2][0] + vec3.y * m[2][1] + vec3.z * m[2][2] + m[2][3],
-            vec3.x * m[3][0] + vec3.y * m[3][1] + vec3.z * m[3][2] + m[3][3]);
+            vec3.x * m_data[0][0] + vec3.y * m_data[0][1] + vec3.z * m_data[0][2] + m_data[0][3],
+            vec3.x * m_data[1][0] + vec3.y * m_data[1][1] + vec3.z * m_data[1][2] + m_data[1][3],
+            vec3.x * m_data[2][0] + vec3.y * m_data[2][1] + vec3.z * m_data[2][2] + m_data[2][3],
+            vec3.x * m_data[3][0] + vec3.y * m_data[3][1] + vec3.z * m_data[3][2] + m_data[3][3]);
     }
 
     bool isInFront(const Vector3D &point) const
     {
-        return point.x * m[0][2] + point.y * m[1][2] + point.z * m[2][2] + m[3][2] > 0;
+        return point.x * m_data[0][2] + point.y * m_data[1][2] + point.z * m_data[2][2] + m_data[3][2] > 0;
     }
 
     bool worldToScreen(const Vector3D &world, Vector3D &screen, int screenWidth, int screenHeight) const
