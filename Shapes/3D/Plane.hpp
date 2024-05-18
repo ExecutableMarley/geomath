36d452c810db156c0a994ab1746e9a391952623f
Marley <marley.arns@yahoo.de>
2024-05-18 20:26:21 +0200

    refactor: Renamed member variables

--- Shapes/3D/Plane.hpp
+++ Shapes/3D/Plane.hpp
@@ -18,34 +18,34 @@ namespace Math
 class Plane
 {
 public:
-    Vector3D planePoint;
-    Vector3D normal;
+    Vector3D m_planePoint;
+    Vector3D m_normal;
 
-    Plane() : planePoint(), normal() {}
+    Plane() : m_planePoint(), m_normal() {}
 
-    Plane(const Vector3D &planePoint, const Vector3D &normal) : planePoint(planePoint), normal(normal) {}
+    Plane(const Vector3D &planePoint, const Vector3D &normal) : m_planePoint(planePoint), m_normal(normal) {}
 
     Plane(const Vector3D &point1, const Vector3D &point2, const Vector3D &point3)
     {
         Vector3D v1 = point2 - point1;
         Vector3D v2 = point3 - point1;
-        normal = v1.cross(v2).normalize();
-        planePoint = point1;
+        m_normal = v1.cross(v2).normalize();
+        m_planePoint = point1;
     }
 
     float distance(const Vector3D &point) const
     {
-        return normal.dot(point - planePoint);
+        return m_normal.dot(point - m_planePoint);
     }
 
     Vector3D project(const Vector3D &point) const
     {
-        return point - normal * distance(point);
+        return point - m_normal * distance(point);
     }
 
     bool isParallel(const Plane &plane) const
     {
-        return normal.isParallel(plane.normal);
+        return m_normal.isParallel(plane.m_normal);
     }
 };
 
