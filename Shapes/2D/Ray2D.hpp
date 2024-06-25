3919b4b769bf91e7bba07af9093670b31d04eb62
Marley <marley.arns@yahoo.de>
2024-06-25 13:44:07 +0200

    feat: implemented ray classes

---
+++ Shapes/2D/Ray2D.hpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) Marley Arns
+ * Licensed under the MIT License.
+*/
+
+#pragma once
+
+#include <math.h>
+
+#include "Geometry/Vector2D.hpp"
+
+namespace Utility
+{
+
+namespace Math
+{
+
+class Ray2D
+{
+public:
+    Vector2D m_origin;
+    Vector2D m_direction;
+
+    Ray2D() : m_origin(), m_direction() {}
+
+    Ray2D(const Vector2D &origin, const Vector2D &direction) : m_origin(origin), m_direction(direction) {}
+
+    Vector2D origin() const
+    {
+        return m_origin;
+    }
+
+    Vector2D direction() const
+    {
+        return m_direction;
+    }
+
+    Vector2D pointAt(float t) const
+    {
+        return m_origin + m_direction * t;
+    }
+
+    float closestParameter(const Vector2D &point) const
+    {
+        return (point - m_origin).dot(m_direction) / m_direction.lengthSquared();
+    }
+};
+
+} // namespace Math
+
+} // namespace Utility
---
+++ Shapes/3D/Ray3D.hpp
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) Marley Arns
+ * Licensed under the MIT License.
+*/
+
+#pragma once
+
+#include <math.h>
+
+#include "Geometry/Vector3D.hpp"
+
+namespace Utility
+{
+
+namespace Math
+{
+
+class Ray3D
+{
+public:
+    Vector3D m_origin;
+    Vector3D m_direction;
+
+    Ray3D() : m_origin(), m_direction() {}
+
+    Ray3D(const Vector3D &origin, const Vector3D &direction) : m_origin(origin), m_direction(direction) {}
+
+    Vector3D origin() const
+    {
+        return m_origin;
+    }
+
+    Vector3D direction() const
+    {
+        return m_direction;
+    }
+
+    Vector3D pointAt(float t) const
+    {
+        return m_origin + m_direction * t;
+    }
+
+    float closestParameter(const Vector3D &point) const
+    {
+        return (point - m_origin).dot(m_direction) / m_direction.lengthSquared();
+    }
+};
+
+
+} // namespace Math
+
+} // namespace Utility
