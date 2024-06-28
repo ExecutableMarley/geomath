6381cfa1e303060bacdc2bd29b0cf02ebe900446
Marley <marley.arns@yahoo.de>
2024-06-28 20:54:33 +0200

    feat: added ray intersection algorithms

--- Shapes/2D/Algorithms2D/Algorithms2D.cpp
+++ Shapes/2D/Algorithms2D/Algorithms2D.cpp
@@ -5,11 +5,23 @@
 
 #include "Algorithms2D.hpp"
 
+#include "CommonMath.hpp"
+#include "../Ray2D.hpp"
+#include "../BBox2D.hpp"
+//#include "Shapes/2D/IShape2D.hpp"
+#include "../Circle.hpp"
+#include "../Triangle.hpp"
+#include "../Rectangle.hpp"
+#include "../Polygon.hpp"
+
+
 namespace Utility
 {
 
 namespace Math
 {
+
+// Distance calculation algorithms
 
 float distancePointToLine(const Vector2D& point, const Vector2D& lineStart, const Vector2D& lineEnd, Vector2D* closestPoint = nullptr)
 {
@@ -73,6 +85,217 @@ float distanceLineToLine(const Vector2D& s1, const Vector2D& s2, const Vector2D&
 float distanceLineToLine(const Line2D& line1, const Line2D& line2, Vector2D* closestPoint1 = nullptr, Vector2D* closestPoint2 = nullptr)
 {
     return distanceLineToLine(line1.m_start, line1.m_end, line2.m_start, line2.m_end, closestPoint1, closestPoint2);
+}
+
+// Intersection calculation algorithms
+
+struct HitInfo2D
+{
+    float t;
+    Vector2D intersectionPoint;
+    //Vector2D normal;
+    //IShape2D* shape;
+};
+
+bool intersectRayWithBBox(const Ray2D& ray, const BBox2D& bbox, float t_min, float t_max, HitInfo2D* hitInfo)
+{
+    for (int i = 0; i < 2; i++)
+    {
+        if (approximatelyZero(ray.m_direction[i]))
+        {
+            if (ray.m_origin[i] < bbox.m_min[i] || ray.m_origin[i] > bbox.m_max[i])
+                return false;
+            continue;
+        }
+
+        float invD = 1.0f / ray.m_direction[i];
+        float t0 = (bbox.m_min[i] - ray.m_origin[i]) * invD;
+        float t1 = (bbox.m_max[i] - ray.m_origin[i]) * invD;
+
+        if (invD < 0.0f)
+            std::swap(t0, t1);
+        
+        t_min = t0 > t_min ? t0 : t_min;
+        t_max = t1 < t_max ? t1 : t_max;
+
+        if (t_max <= t_min)
+            return false;
+    }
+
+    if (hitInfo)
+    {
+        hitInfo->t = t_min;
+        hitInfo->intersectionPoint = ray.m_origin + ray.m_direction * t_min;
+    }
+
+    return true;
+}
+
+bool intersectRayWithCircle(const Ray2D& ray, const Circle& circle, float t_min, float t_max, HitInfo2D* hitInfo)
+{
+    const Vector2D oc = ray.m_origin - circle.m_center;
+    const float a = ray.m_direction.dot(ray.m_direction);
+    const float b = 2.0f * oc.dot(ray.m_direction);
+    const float c = oc.dot(oc) - circle.m_radius * circle.m_radius;
+    const float discriminant = b * b - 4 * a * c;
+
+    if (discriminant < 0)
+        return false;
+
+    const float sqrtDiscriminant = sqrt(discriminant);
+    float t = (-b - sqrtDiscriminant) / (2.0f * a);
+    if (t < t_min || t > t_max)
+    {
+        t = (-b + sqrtDiscriminant) / (2.0f * a);
+        if (t < t_min || t > t_max)
+            return false;
+    }
+
+    if (hitInfo)
+    {
+        hitInfo->t = t;
+        hitInfo->intersectionPoint = ray.pointAt(t);
+    }
+
+    return true;
+}
+
+bool intersectRayWithTriangle(const Ray2D& ray, const Triangle& triangle, float t_min, float t_max, HitInfo2D* hitInfo)
+{
+    const Vector2D edge1 = triangle.m_b - triangle.m_a;
+    const Vector2D edge2 = triangle.m_c - triangle.m_a;
+
+    // Calculate determinant
+    const Vector2D h = { -ray.m_direction.y, ray.m_direction.x };
+    const float det = edge1.dot(h);
+
+    if (approximatelyZero(det))
+        return false;
+
+    const float invDet = 1.0f / det;
+
+    const Vector2D s = ray.m_origin - triangle.m_a;
+    const float u = s.dot(h) * invDet;
+
+    if (u < 0.0f || u > 1.0f)
+        return false;
+
+    const Vector2D q = { -s.y, s.x };
+    const float v = ray.m_direction.dot(q) * invDet;
+
+    if (v < 0.0f || u + v > 1.0f)
+        return false;
+
+    const float t = edge2.dot(q) * invDet;
+
+    if (t < t_min || t > t_max)
+        return false;
+
+    if (hitInfo) 
+    {
+        hitInfo->t = t;
+        hitInfo->intersectionPoint = ray.pointAt(t);
+    }
+
+    return true;
+}
+
+bool intersectRayWithSegment(const Ray2D& ray, const Vector2D& p1, const Vector2D& p2, float t_min, float t_max, HitInfo2D* hitInfo)
+{
+    Vector2D v1 = ray.m_origin - p1;
+    Vector2D v2 = p2 - p1;
+    Vector2D v3(-ray.m_direction.y, ray.m_direction.x);
+
+    float dot = v2.dot(v3);
+    
+    if (approximatelyZero(dot))
+        return false; // Parallel
+
+    float t1 = v2.cross(v1) / dot;
+    float t2 = v1.dot(v3) / dot;
+
+    if (t1 < 0 || t2 < 0 || t2 > 1)
+        return false;
+
+    if (t1 >= t_min && t1 <= t_max)
+    {
+        if (hitInfo)
+        {
+            hitInfo->t = t1;
+            hitInfo->intersectionPoint = ray.m_origin + ray.m_direction * t1;
+        }
+        return true;
+    }
+
+    return false;
+}
+
+bool intersectRayWithRectangle(const Ray2D& ray, const Rectangle& rectangle, float t_min, float t_max)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        const Vector2D& p1 = rectangle.vertexAt(i);
+        const Vector2D& p2 = rectangle.vertexAt((i + 1) % 4);
+
+        if (intersectRayWithSegment(ray, p1, p2, t_min, t_max, NULL))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool intersectRayWithRectangle(const Ray2D& ray, const Rectangle& rectangle, float t_min, float t_max, HitInfo2D* hitInfo)
+{
+    bool hit = false;
+    float t = t_max;
+
+    for (int i = 0; i < 4; i++)
+    {
+        const Vector2D& p1 = rectangle.vertexAt(i);
+        const Vector2D& p2 = rectangle.vertexAt((i + 1) % 4);
+
+        if (intersectRayWithSegment(ray, p1, p2, t_min, t, hitInfo))
+        {
+            hit = true;
+            t = hitInfo->t;
+        }
+    }
+    return hit;
+}
+
+bool intersectRayWithPolygon(const Ray2D& ray, const Polygon& polygon, float t_min, float t_max)
+{
+    for (int i = 0; i < polygon.vertexCount(); i++)
+    {
+        const Vector2D& p1 = polygon.vertexAt(i);
+        const Vector2D& p2 = polygon.vertexAt((i + 1) % 4);
+
+        if (intersectRayWithSegment(ray, p1, p2, t_min, t_max, NULL))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool intersectRayWithPolygon(const Ray2D& ray, const Polygon& polygon, float t_min, float t_max, HitInfo2D* hitInfo)
+{
+    bool hit = false;
+    float t = t_max;
+
+    for (int i = 0; i < polygon.vertexCount(); i++)
+    {
+        const Vector2D& p1 = polygon.vertexAt(i);
+        const Vector2D& p2 = polygon.vertexAt((i + 1) % 4);
+
+        if (intersectRayWithSegment(ray, p1, p2, t_min, t_max, hitInfo))
+        {
+            hit = true;
+            t = hitInfo->t;
+        }
+    }
+    return hit;
 }
 
 } // namespace Math
