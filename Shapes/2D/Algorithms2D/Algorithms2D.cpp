28ad56dfc88699d42b837535990afa9bc3631c33
Marley <marley.arns@yahoo.de>
2025-06-14 17:00:58 +0200

    fix: redefinitions of parameters

--- Shapes/2D/Algorithms2D/Algorithms2D.cpp
+++ Shapes/2D/Algorithms2D/Algorithms2D.cpp
@@ -44,7 +44,7 @@ bool isPointOnSegment(const Vector2D& point, const Line2D& line)
 
 // Distance calculation algorithms
 
-float distancePointToLine(const Vector2D& point, const Vector2D& lineStart, const Vector2D& lineEnd, Vector2D* closestPoint = nullptr)
+float distancePointToLine(const Vector2D& point, const Vector2D& lineStart, const Vector2D& lineEnd, Vector2D* closestPoint)
 {
     return distancePointToLine(point, Line2D(lineStart, lineEnd), closestPoint);
 
@@ -57,12 +57,12 @@ float distancePointToLine(const Vector2D& point, const Vector2D& lineStart, cons
     return (point - projection).length();
 }
 
-float distancePointToLine(const Vector2D& point, const Line2D& line, Vector2D* closestPoint = nullptr)
+float distancePointToLine(const Vector2D& point, const Line2D& line, Vector2D* closestPoint)
 {
     return distancePointToLine(point, line.m_start, line.m_end, closestPoint);
 }
 
-float distanceLineToLine(const Vector2D& s1, const Vector2D& s2, const Vector2D& k1, const Vector2D& k2, Vector2D* closestPoint1 = nullptr, Vector2D* closestPoint2 = nullptr)
+float distanceLineToLine(const Vector2D& s1, const Vector2D& s2, const Vector2D& k1, const Vector2D& k2, Vector2D* closestPoint1 = nullptr, Vector2D* closestPoint2)
 {
     const Vector2D delta1 = s2 - s1;
     const Vector2D delta2 = k2 - k1;
@@ -103,7 +103,7 @@ float distanceLineToLine(const Vector2D& s1, const Vector2D& s2, const Vector2D&
     return (closestPointOnLine1 - closestPointOnLine2).length();
 }
 
-float distanceLineToLine(const Line2D& line1, const Line2D& line2, Vector2D* closestPoint1 = nullptr, Vector2D* closestPoint2 = nullptr)
+float distanceLineToLine(const Line2D& line1, const Line2D& line2, Vector2D* closestPoint1 = nullptr, Vector2D* closestPoint2)
 {
     return distanceLineToLine(line1.m_start, line1.m_end, line2.m_start, line2.m_end, closestPoint1, closestPoint2);
 }
