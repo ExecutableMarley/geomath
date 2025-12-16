a9459feacc4f50423118edf661c142fdba8e529b
Marley <marley.arns@yahoo.de>
2025-12-16 18:36:10 +0100

    fix: Now structured identically to 2D

--- Shapes/3D/IShape3D.hpp
+++ Shapes/3D/IShape3D.hpp
@@ -9,6 +9,7 @@
 
 #include "Geometry/Vector3D.hpp"
 #include "BBox3D.hpp"
+#include "Ray3D.hpp"
 
 namespace Utility
 {
@@ -26,6 +27,7 @@ enum ShapeType3D
     MESH
 };
 
+/*
 class IShape3D
 {
 public:
@@ -45,7 +47,29 @@ public:
     //virtual bool intersects(const IShape3D &shape) const = 0;
 
     virtual BBox3D boundingBox() const = 0;
+};*/
+
+//Todo: Implement this
+class IBaseShape3D
+{
+    virtual ShapeType3D type() const = 0;
+
+    virtual IBaseShape3D& translate(const Vector3D &translation) = 0;
+
+    virtual bool contains(const Vector3D &point) const = 0;
 };
+
+class IFiniteShape3D : IBaseShape3D
+{
+    virtual float volume() const = 0;
+
+    virtual float surfaceArea() const = 0;
+
+    virtual Vector3D centroid() const = 0;
+
+    virtual BBox3D boundingBox() const = 0;
+};
+//
 
 } // namespace Math
 
