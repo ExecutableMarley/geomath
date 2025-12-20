e39489e0846b9846086bc21e60bedc4c47a52c9f
Marley <marley.arns@yahoo.de>
2025-12-20 02:44:47 +0100

    Improved class by changing some internal logic. Should now properly approximate floating values

--- Algebra/Fraction.hpp
+++ Algebra/Fraction.hpp
@@ -4,6 +4,8 @@
 #include <cstdint>
 #include <stdexcept>
 
+#include "../CommonMath.hpp"
+
 namespace Arns
 {
 
@@ -27,35 +29,26 @@ public:
         simplify();
     }
 
-    Fraction(float decimal, int maxPrecision)
+    double decimal()
     {
-        int64_t num = floor(decimal * maxPrecision);
-        this->numerator = num;
-        this->denominator = maxPrecision;
-        this->simplify();
+        return (double)numerator / (double)denominator;
     }
 
-    Fraction(double decimal, int maxPrecision)
+    Fraction& simplify()
     {
-        int64_t num = floor(decimal * maxPrecision);
-        this->numerator = num;
-        this->denominator = maxPrecision;
-        this->simplify();
-    }
+        if (denominator < 0)
+        {
+            numerator   = -numerator;
+            denominator = -denominator;
+        }
 
-    float decimal()
-    {
-        return (float)numerator / (float)denominator;
-    }
-
-    void simplify()
-    {
         int g = gcd(numerator, denominator);
         if (g != 1)
         {
             this->numerator   /= g;
             this->denominator /= g;
         }
+        return *this;
     }
 
     Fraction createSimplified() const
@@ -140,12 +133,65 @@ public:
         return a.numerator != b.numerator || a.denominator != b.denominator;
     }
 
-
-
-
-    static Fraction fromDecimal(float decimal)
+    //Todo: Improve
+    static Fraction fromDecimal(double decimal, int maxPrecision)
     {
-        return Fraction(decimal, 10);
+        int64_t num = floor(decimal * maxPrecision);
+        return Fraction(num, (int64_t)maxPrecision).simplify();
+    }
+
+    //Todo: Can this fail from normal input?
+    static Fraction fromDecimal(double value, double tolerance = 1e-9)
+    {
+        bool negative = value < 0;
+        value = std::abs(value);
+
+        if (std::abs(value - std::round(value)) <= tolerance)
+        return Fraction(negative ? -static_cast<int64_t>(std::llround(value))
+                                 : static_cast<int64_t>(std::llround(value)),
+                        (int64_t)1);
+
+        double x = value;
+        int64_t a = static_cast<int64_t>(std::floor(x));
+
+        int64_t h0 = 0, h1 = 1;
+        int64_t k0 = 1, k1 = 0;
+
+        int64_t h2 = a, k2 = 1;
+
+        constexpr size_t MAX_ITER = 100;
+        for (size_t i = 0; i < MAX_ITER; ++i)
+        {
+            double approx = double(h2) / k2;
+            /*
+            if (std::abs(value - approx) <= tolerance * std::max(1.0, value))
+                break;
+                */
+            if (approximatelyEqual(value, approx, tolerance, tolerance))
+                break;
+
+            double frac = x - a;
+            if (approximatelyZero(frac))
+                break;
+
+            x = 1.0 / frac;
+            a = static_cast<int64_t>(std::floor(x));
+
+            h0 = h1; h1 = h2;
+            k0 = k1; k1 = k2;
+
+            //Overflow check
+            if (std::abs(a) > (INT64_MAX - std::abs(h0)) / std::abs(h1))
+                break;
+
+            if (std::abs(a) > (INT64_MAX - std::abs(k0)) / std::abs(k1))
+                break;
+
+            h2 = a * h1 + h0;
+            k2 = a * k1 + k0;
+        }
+        //Probably already simplified?
+        return Fraction(negative ? -h2 : h2, k2).simplify();
     }
 
     //Todo: Force specific denominator
