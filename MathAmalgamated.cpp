a8eb802ad2ea9fefd452746843c6dc4a18be6fd2
Marley <marley.arns@yahoo.de>
2025-12-21 16:32:54 +0100

    feat: Added amalgamation source file

---
+++ MathAmalgamated.cpp
@@ -0,0 +1,37 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) Marley Arns
+ * Licensed under the MIT License.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * MathAmalgamated.cpp
+ * This file is used solely for amalgamation purposes.
+*/
+
+//2D
+#include "Shapes/2D/Algorithms2D/Algorithms2D.cpp"
+
+#include "Shapes/2D/Algorithms2D/Delaunay/DelaunayTriangulation.cpp"
+
+//3D
+#include "Shapes/3D/Algorithms3D/Algorithms3D.cpp"
