383d66644d5c746466fb2e5aaed36ef050d26a1e
Marley <marley.arns@yahoo.de>
2026-03-23 17:06:30 +0100

    tests: added basic tests

---
+++ tests/test_common_math.cpp
@@ -0,0 +1,77 @@
+#include "third_party/doctest.h"
+
+#include "../CommonMath.hpp"
+
+using namespace Arns::Math;
+
+TEST_CASE("approximatelyZero(float)")
+{
+    constexpr float eps = FloatAbsEpsilon;
+
+    CHECK(approximatelyZero(0.0f));
+    CHECK(approximatelyZero(+eps * 0.5f));
+    CHECK(approximatelyZero(-eps * 0.5f));
+
+    CHECK_FALSE(approximatelyZero(+eps * 1.1f));
+    CHECK_FALSE(approximatelyZero(-eps * 1.1f));
+
+    CHECK_FALSE(approximatelyZero(1.0f));
+    CHECK_FALSE(approximatelyZero(-1.0f));
+}
+
+TEST_CASE("approximatelyZero(double)")
+{
+    constexpr double eps = DoubleAbsEpsilon;
+
+    CHECK(approximatelyZero(0.0));
+    CHECK(approximatelyZero(+eps * 0.5));
+    CHECK(approximatelyZero(-eps * 0.5));
+
+    CHECK_FALSE(approximatelyZero(+eps * 1.1));
+    CHECK_FALSE(approximatelyZero(-eps * 1.1));
+
+    CHECK_FALSE(approximatelyZero(1.0));
+    CHECK_FALSE(approximatelyZero(-1.0));
+}
+
+TEST_CASE("approximatelyEqual - absolute epsilon near zero (float)")
+{
+    float a = 0.0f;
+    float b = FloatAbsEpsilon * 0.9f;
+
+    CHECK(approximatelyEqual(a, b));
+    CHECK(approximatelyEqual(b, a));
+}
+
+TEST_CASE("approximatelyEqual - absolute epsilon near zero (double)")
+{
+    double a = 0.0;
+    double b = DoubleAbsEpsilon * 0.9;
+
+    CHECK(approximatelyEqual(a, b));
+    CHECK(approximatelyEqual(b, a));
+}
+
+TEST_CASE("approximatelyEqual - relative epsilon large magnitude (float)")
+{
+    float a = 1e6f;
+    float b = a + a * FloatRelEpsilon * 0.9f;
+
+    CHECK(approximatelyEqual(a, b));
+    CHECK(approximatelyEqual(b, a));
+}
+
+TEST_CASE("approximatelyEqual - relative epsilon large magnitude (double)")
+{
+    double a = 1e12;
+    double b = a + a * DoubleRelEpsilon * 0.9;
+
+    CHECK(approximatelyEqual(a, b));
+    CHECK(approximatelyEqual(b, a));
+}
+
+TEST_CASE("approximatelyEqual - clearly unequal")
+{
+    CHECK_FALSE(approximatelyEqual(1.0f, 1.1f));
+    CHECK_FALSE(approximatelyEqual(1.0, 1.1));
+}
---
+++ tests/test_vector2d.cpp
@@ -0,0 +1,22 @@
+#include "third_party/doctest.h"
+
+#include "../Geometry/Vector2D.hpp"
+
+using namespace Arns::Math;
+
+TEST_CASE("Vector2D addition")
+{
+    Vector2D a{1, 2};
+    Vector2D b{3, 4};
+
+    Vector2D c = a + b;
+    CHECK(c.x == doctest::Approx(4));
+    CHECK(c.y == doctest::Approx(6));
+}
+
+TEST_CASE("Vector2D length")
+{
+    Vector2D v{3, 4};
+
+    CHECK(v.length() == doctest::Approx(5));
+}
